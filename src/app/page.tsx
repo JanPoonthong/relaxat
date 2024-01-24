07ae53a83daece03fd733f49f21da9589abe64f9
Jan Poonthong <janpoonthong628@gmail.com>
2024-01-24 18:57:30 +0700

    feat: redirect to /financial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+"use client"
+
 import SideBar from "./ui/home/side-bar";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-    return (
-        <>
-            <main>
-                <SideBar />
-            </main>
-        </>
-    );
+    const router = useRouter();
+    router.push('/financial');
+    return null;
 }
