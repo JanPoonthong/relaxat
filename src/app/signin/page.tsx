2e92ad732614beb3ed324f80709ad8abcc5aba30
Jan Poonthong <janpoonthong628@gmail.com>
2024-02-17 14:32:26 +0700

    feat: link signup and singin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -81,7 +81,7 @@ export default function Page() {
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Not a member?{" "}
                         <a
-                            href="#"
+                            href="/signup"
                             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                         >
                             Sign Up
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -91,7 +91,7 @@ export default function Page() {
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Already have an account?{" "}
                         <a
-                            href="#"
+                            href="/signin"
                             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                         >
                             Sign In
