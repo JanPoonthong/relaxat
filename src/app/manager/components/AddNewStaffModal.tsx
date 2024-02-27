dc2934dfca3d3d060190ac31ae4029c2f0e1e98f
Kaung Myat Htet <kaungmyathtet.dev@gmail.com>
2024-02-27 20:38:20 +0700

    add start and end shift time in staff form

--- src/app/manager/components/AddNewStaffModal.tsx
+++ src/app/manager/components/AddNewStaffModal.tsx
@@ -28,7 +28,7 @@ export default function AddNewStaffModal({
                             as="h3"
                             className="text-lg font-medium leading-6 text-gray-900"
                         >
-                            Add New Employee
+                            Add New Staff
                         </Dialog.Title>
                         <div className="mt-2">
                             <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -115,6 +115,43 @@ export default function AddNewStaffModal({
                                     </div>
                                 </div>
                             </div>
+                            <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-3">
+                                    <label
+                                        htmlFor="first-name"
+                                        className="block text-sm font-medium leading-6 text-gray-900"
+                                    >
+                                        Start Shift Time
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            type="number"
+                                            name="start-time"
+                                            id="start-time"
+                                            min="1" max="24"
+                                            className="block w-full rounded-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="sm:col-span-3">
+                                    <label
+                                        htmlFor="first-name"
+                                        className="block text-sm font-medium leading-6 text-gray-900"
+                                    >
+                                        End Shift Time
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            type="number"
+                                            name="end-time"
+                                            id="end-time"
+                                            min="1" max="24"
+                                            className="block w-full rounded-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
                             <div className="mt-2">
                                 <label
                                     htmlFor="service"
@@ -143,7 +180,7 @@ export default function AddNewStaffModal({
                                 />
                             </div>
                         </div>
-                        <hr className="mt-3" />
+                        <hr className="mt-3"/>
                         <div className="mt-2 float-end">
                             <button
                                 type="button"
@@ -157,7 +194,7 @@ export default function AddNewStaffModal({
                                 className="inline-flex justify-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
                                 onClick={() => onConfirm()}
                             >
-                                Add New Employee
+                                Add New Staff
                             </button>
                         </div>
                     </Dialog.Panel>
