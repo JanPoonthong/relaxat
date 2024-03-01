94de1edf4ae9bb94f2b61b2a59709060ba3a2604
Jan Poonthong <janpoonthong628@gmail.com>
2024-03-01 11:25:30 +0700

    style: format code with prettier

--- src/app/manager/components/AddNewStaffModal.tsx
+++ src/app/manager/components/AddNewStaffModal.tsx
@@ -128,7 +128,8 @@ export default function AddNewStaffModal({
                                             type="number"
                                             name="start-time"
                                             id="start-time"
-                                            min="1" max="24"
+                                            min="1"
+                                            max="24"
                                             className="block w-full rounded-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     </div>
@@ -146,7 +147,8 @@ export default function AddNewStaffModal({
                                             type="number"
                                             name="end-time"
                                             id="end-time"
-                                            min="1" max="24"
+                                            min="1"
+                                            max="24"
                                             className="block w-full rounded-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     </div>
@@ -180,7 +182,7 @@ export default function AddNewStaffModal({
                                 />
                             </div>
                         </div>
-                        <hr className="mt-3"/>
+                        <hr className="mt-3" />
                         <div className="mt-2 float-end">
                             <button
                                 type="button"
